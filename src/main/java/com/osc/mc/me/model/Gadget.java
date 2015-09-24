e6cd3ddfdd6d21ae47277b0da8205d1de3452eb7
HiepDT <hiepdt0109@gmail.com>
2015-09-24 14:11:51 +0700

    Added Gadget.java

---
+++ src/main/java/com/osc/mc/me/model/Gadget.java
@@ -0,0 +1,15 @@
+package com.osc.mc.me.model;
+
+public class Gadget {
+    //DI
+    String description;
+
+    public Gadget(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Autowired shit. It says: "+description;
+    }
+}
